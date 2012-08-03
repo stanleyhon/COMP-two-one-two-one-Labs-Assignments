7ede7d6f2bc8b31a627469ec3842bd61beb8af30
stanleyhon <stanleyhon348@gmail.com>
2012-08-02 22:29:11 -0700

    Update AVR Workspace/lab1/lab1a.asm
    Actually implemented a=a-b when a>b, and b=b-a when b>a.
    Fixed non-two byte-edness.

--- AVR Workspace/lab1/lab1a.asm
+++ AVR Workspace/lab1/lab1a.asm
@@ -2,7 +2,7 @@
 ;void main(void){
 ;   while(a!=b){
 ;      if(a>b){
-;	     a=a-b;
+;       a=a-b;
 ;      else
 ;         b=b-a;
 ;      }
@@ -10,21 +10,36 @@
 ;}
 
 .include "m64def.inc"
-.def a =r16 
-.def b =r17
-ldi a, 1
-ldi b, 10 
+.def a_high =r16 
+.def a_low =r17
+.def b_high =r18
+.def b_low =r19
 
+; Theoretically it should look like
+; 0000 0001 0000 0010
+; which represents 257 
+ldi a_high, high (6969)
+ldi a_low, low (6969)
 
-while_start: cp a, b
-breq while_end
+; Theoretically it should look like
+; 0000 0001 0000 0001
+; which represents 256
+ldi b_high, high (1)
+ldi b_low, low (1)
 
-cp a, b
-brlt skip_if
-sub a, b
+while_start: cp a_low, b_low
+cpc a_high, b_high
+breq while_end ; CHECK FOR EQUALITY
+
+cp a_low, b_low
+cpc a_high, b_high
+brlt skip_if ; 
+sub b_low, a_low
+sbc b_high, a_high
 jmp skip_else
 
-skip_if: sub b, a
+skip_if: sub a_low, b_low
+sbc a_high, b_high
 skip_else: jmp while_start
 while_end: jmp end
 
