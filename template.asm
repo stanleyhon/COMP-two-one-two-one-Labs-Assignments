c831412fc5ecaab4a0e4b2cfc200850520cdf1bb
Christopher Tin-Loi <naiteikite@gmail.com>
2012-08-12 13:56:40 +1000

    Update template.asm
    STANLEY IS A BAD

--- template.asm
+++ template.asm
@@ -1,1 +1,19 @@
-CTL IS BAD
+
+; var = r15
+; var = r16
+; var = r17
+; var = r18
+; var = r19
+; var = r20
+; var = r21
+; var = r22
+; var = r23
+; var = r24
+; var = r25
+; var = r26 x 
+; var = r27 x
+; var = r28 y 
+; var = r29 y
+; var = r30 z
+; var = r31 z
+
