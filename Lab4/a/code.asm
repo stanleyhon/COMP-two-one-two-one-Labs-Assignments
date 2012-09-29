fa15d4fb876f63a2b69e1413ecc246def642023b
Christopher Tin-Loi <naiteikite@gmail.com>
2012-09-29 14:11:04 +1000

    Update Lab4/a/code.asm
    Replaced with non-shit code

--- Lab4/a/code.asm
+++ Lab4/a/code.asm
@@ -1,8 +1,16 @@
-.include "m64def.inc"
 
-.def temp1 = r16
-; def temp2 = r17
-;.def var = r18
+; LCD D0-D3 -> PB0-PB3
+; LCD D4-D7 -> PD4-PD7
+; LCD BE-RS -> PA0-PA3
+; Keypad R0-C3 -> PC0-PC7
+; Mot -> PB4
+; OpD -> PD0
+; PB0 (pushbutton) -> OpE (just to drive the ammeter)
+
+.include "m64def.inc"
+.def temp = r16
+;.def temp2 = r17
+;.def temp3 = r18
 ;.def var = r19
 ;.def var = r20
 ;.def var = r21
@@ -17,52 +25,72 @@
 ;.def var = r30 z
 ;.def var = r31 z
 
-jmp reset 
-jmp Timer0 ; interrupt handler for timer0 (overflow)
-
 .dseg
-
-motorSpeed: .byte 1
 
 .cseg
 
+//Not sure if default interrupt handler is necessary
+jmp reset
+jmp EXT_INT0 ; ext int0
+jmp Default ; IRQ1 Handler
+jmp Default ; IRQ2 Handler
+jmp Default ; IRQ3 Handler
+jmp Default ; IRQ4 Handler
+jmp Default ; IRQ5 Handler
+jmp Default ; IRQ6 Handler
+jmp Default ; IRQ7 Handler
+jmp Default ; Timer2 Compare Handler
+jmp Default ; Timer2 Overflow Handler
+jmp Default ; Timer1 Capture Handler
+jmp Default ; Timer1 CompareA Handler
+jmp Default ; Timer1 CompareB Handler
+jmp Default ; Timer1 Overflow Handler
+jmp Default ; Timer0 Compare Handler
+jmp Timer0  ; Timer0 Overflow Handler
+
+Default:
+reti
+
 reset:
-	ldi temp1, low(RAMEND)
-	out SPL, temp1
-	ldi temp1, high(RAMEND)
-	out SPH, temp1 
-/*
-   ser temp1
 
-   ldi temp1, (2 << ISC10) | (2 << ISC00) ;setting the interrupts for falling edge
-	sts EICRA, temp1 ;storing them into EICRA
-	in temp1, EIMSK ;taking the values inside the EIMSK
-	ori temp1, (1<<INT0) | (1<<INT1) ; oring the values with INT0 and INT1
-	out EIMSK, temp1 ; enabling interrput0 and interrupt1
+	//setup stack
+	ldi YH,high(RAMEND)    ;RAMEND is the  highest SRAM address
+	ldi YL,low(RAMEND)     ;setting the pointers to SRAM RAMEND address
+	out SPH,YH             ;setting the stack pointers SP high 
+	out SPL,YL             ;setting the stack pointers SP low
 
-   sei // enable global? interrupts
+	/*EX_INT0	
+	in temp, EIMSK
+	ldi temp, (2<<ISC00)	//Set for falling edge (EXT_INT0)				
+	sts EICRA, temp                       
 
-	ldi ZL, low(motorSpeed)
-	ldi ZH, high(motorSpeed)
+	in temp, EIMSK                         
+	ori temp, (1<<INT0)		//Enable EXT_INT0   			
+	out EIMSK, temp                       
+	*/
 
-	ldi temp1, 20
-	st Z, temp1
-*/
-	ldi temp1, 0b011011112
-	out TCCR0, temp1
-   
-	ldi temp1,           
-	out OCR0, temp1
+	//Enable global interrupts
+	sei
 
-	ldi temp1, 1<<TOIE0       ; =278 microseconds
-	out TIMSK, temp1          ; T/C0 interrupt enable
+	ldi temp, (1<<WGM01) | (1<<WGM00) | (2<<COM00) | (2<<CS00)
+	/*	WGM01 + WGM00 = NO force output compare (for PWN modes)
+		COM00 = Clear OC0 on Compare Match when up-counting. Set OC0 on Compare Match when downcounting.
+		CS00 = Prescaler = 8
+	*/
+	out TCCR0, temp
 
-   ser temp1
+	//Set OCR0, Roughly 3 x (RPS+10) = OCR0 (add 10 for ramp up)
+	ldi temp, 0xA4 
+	out OCR0, temp
 
-	out DDRB, temp1 ; PORTB, the data port is usually all otuputs
+	//Timer0 interrupt enable
+	ldi temp, (1<<TOIE0)
+	out TIMSK, temp  
 
-chill:
-	rjmp chill
+main: rjmp main
+
+EXT_INT0:
+reti
 
 Timer0:
-	reti
+reti
